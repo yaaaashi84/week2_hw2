2bf27ae7aa4854ff8dd57136da431e38dfb299d8
yaaashi84 <yaaaashi.84@gmail.com>
2022-11-18 04:55:23 +0900

    jamanabunkeshita

--- app.py
+++ app.py
@@ -19,17 +19,3 @@ font = ImageFont.truetype(font_type, 60)
 img2 = Image.open(img2_path)
 ImageDraw.Draw(img2).text((150, 150), text, font=font, fill=(0, 255, 255))
 img2.save('images/draw.png')
-
-
-# size = font.getsize(text)
-
-# draw.text((resized_img[0] - size[0], resized_image[1] - size[1], text, font, '#0FF'))
-
-# today = str(atetime.date.today())
-# today = (today.replace('-', ''))
-
-# output_path = f"images/{today}+out.jpg"
-
-# img.save(output_path)
-
-
